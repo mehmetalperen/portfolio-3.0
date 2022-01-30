9e035621b8eda5a2a021442d383bc77fdddf7cfa
mehmetalperen <mhmtalperennadi@gmail.com>
2022-01-29 22:59:34 -0800

    finish navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <div className="App">fsdfsfs</div>;
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+import { Link } from "react-scroll";
+
+export default function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="logo-wrapper">
+        <a className="logo-link">
+          <Link to="home" smooth={true} duration={1000}>
+            <h1 className="logo">Mehmet Nadi</h1>
+          </Link>
+        </a>
+      </div>
+
+      <div className="navigation-wrapper">
+        <a href="" className="navigation">
+          <Link to="work-section" smooth={true} duration={1000}>
+            Work
+          </Link>
+        </a>
+        <a href="" className="navigation">
+          <Link to="contact-section" smooth={true} duration={1000}>
+            Contact
+          </Link>
+        </a>
+      </div>
+      <style jsx>{`
+        .navbar {
+          overflow: auto;
+            display: flex;
+            justify-content: space-between;
+            padding: 1.2rem 0;
+        }
+       
+       
+        .logo {
+          font-family: Karla;
+          font-style: normal;
+          font-weight: bold;
+          font-size: 30px;
+          line-height: 35px;
+        }
+        .navigation {
+            font-family: Karla;
+            font-style: normal;
+            font-weight: bold;
+            font-size: 20px;
+            line-height: 23px;
+            margin: 0 10px;
+        }
+
+
+        .navigation-wrapper{
+
+          min-width: 200px;
+          width: 22%;
+          display: flex;
+          flex-wrap: nowrap;
+          justify-content: space-between;
+        }
+
+        @media only screen and (max-width: 650px) {
+  body {
+    padding: 0;
+  }
+  .logo-wrapper, .navigation-wrapper {
+      margin: 0 30px;
+  }
+}
+
+@media only screen and (max-width: 550px) {
+  .navigation-wrapper {
+    display: none;
+  }
+}
+
+
+}
+      `}</style>
+    </div>
+  );
+}
