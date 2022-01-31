a0b28d613aecc88e4e54d30497657f4afc414a2f
mehmetalperen <mhmtalperennadi@gmail.com>
2022-01-30 19:47:48 -0800

    finish contatct section

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import "./App.css";
 import Home from "./components/Home";
 import MyWorkSection from "./components/MyWorkSection";
 import Navbar from "./components/Navbar";
-
+import Contact from "./components/Contact";
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Home />
       <MyWorkSection />
+      <Contact />
     </div>
   );
 }
