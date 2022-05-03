852decb89dc4b68cb21beefa5beaa8a4080ec201
mehmetalperen <mhmtalperennadi@gmail.com>
2022-05-03 09:34:08 -0700

    trying to fix the img size problem

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -106,10 +106,7 @@ export default function Home() {
             justify-content: center;
           }
           .img-wrapper {
-            width: 100%;
             margin: 0 0 50px 0;
-            display: flex;
-            justify-content: center;
           }
           .greeding-wrapper {
             min-width: 100%;
