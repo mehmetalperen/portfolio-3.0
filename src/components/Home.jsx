7b6fecfbfc784ef0c2fed6ce0e325da1df744461
mehmetalperen <mhmtalperennadi@gmail.com>
2022-05-03 09:29:13 -0700

    trying to fix the img problem

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -45,11 +45,15 @@ export default function Home() {
           align-items: flex-end;
           min-width: fit-content;
           margin: 5% 0;
+          border-color: blue;
+          border-width: 2px;
         }
 
         .img-wrapper {
           margin: 0 5% 0 0;
           min-width: 250px;
+          border-color: orange;
+          border-width: 2px;
         }
 
         .greeding-wrapper {
@@ -57,6 +61,8 @@ export default function Home() {
         }
         .pic {
           max-width: 250px;
+          height: auto;
+          border-width: 2px;
         }
 
         .greeting-title {
